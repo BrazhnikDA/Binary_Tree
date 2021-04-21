ab03cfebe4cc8585203aedcd5e19d449e489d74b
BrazhnikDA <dimity.brazhnik@yandex.ru>
2021-04-21 17:50:35 +0300

    Реализация main и функций класса

--- Tree/CNode.h
+++ Tree/CNode.h
@@ -7,4 +7,91 @@ private:
 	int count;
 	std::string value;
 	CNode* left, * right;
+public:
+	CNode(std::string val, CNode* l, CNode* r);
+	CNode* addNode(CNode* tree, std::string val);
+
+	CNode* deleteNode(CNode* tree, CNode* node);
+
+	CNode* min(CNode* tree);
+	CNode* max(CNode* tree);
+
+	void PrintTree(CNode* root);
 };
+
+inline CNode::CNode(std::string val, CNode* l, CNode* r)
+{
+	value = val;
+	left = l;
+	right = r;
+	count = 1;
+}
+
+inline CNode* CNode::addNode(CNode* tree, std::string val)
+{
+	if (tree == nullptr)
+	{
+		tree = new CNode(val, nullptr, nullptr);
+		tree->value = val;
+		tree->left = nullptr;
+		tree->right = nullptr;
+		tree->count = 1;
+	}
+	else
+	{
+		if (tree->value > val)
+		{
+			tree->left = addNode(tree->left, val);
+		}
+		else
+		{
+			if (tree->value < val)
+			{
+				tree->right = addNode(tree->right, val);
+			}
+		}
+	}
+
+	return tree;
+}
+
+inline CNode* CNode::deleteNode(CNode* tree, CNode* node)
+{
+	return NULL;
+}
+
+inline CNode* CNode::min(CNode* tree)
+{
+	if (tree != nullptr)
+	{
+		while (tree->left != nullptr)
+		{
+			tree = tree->left;
+		}
+	}
+	std::cout << "\nMIN: " << tree->value << "\n";
+	return tree;
+}
+
+inline CNode* CNode::max(CNode* tree)
+{
+	if (tree != nullptr)
+	{
+		while (tree->right != nullptr)
+		{
+			tree = tree->right;
+		}
+	}
+	std::cout << "\nMAX: " << tree->value << "\n";
+	return tree;
+}
+
+inline void CNode::PrintTree(CNode* root)
+{
+	if (root != nullptr)
+	{
+		std::cout << root->value << " ";
+		PrintTree(root->right);
+		PrintTree(root->left);
+	}
+}
--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,10 +1,51 @@
 #include "CNode.h"
 #include <locale>
 
+#define SizeSlovar 20
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 	
+	char slovar[SizeSlovar] = { ',', '.', '?', '!', ':', '(', ')', ';', '{', '}', '1','2','3','4','5','6','7','8','9','0' };
+	std::string in = "шло 20 апреля. я всё ещё не знал программирование";
+
+	int count = 0;
+	int countNum = 0;
+	std::string res[100];
+
+	bool IsGood = true;
+	for (int i = 0; i < in.size(); i++)
+	{
+		IsGood = true;
+		if (in[i] == ' ') { count++; continue; }
+		for (int j = 0; j < SizeSlovar; j++)
+		{
+			if (in[i] == slovar[j])
+			{
+				if (j > 10 && countNum < 1) { count--; countNum++; }
+				IsGood = false;
+				break;
+			}
+		}
+		if (IsGood)
+		{
+			countNum = 0;
+			res[count] += in[i];
+		}
+	}
+
+	CNode c(res[0], nullptr, nullptr);
+	for (int i = 1; i < 9; i++)
+	{
+		c.addNode(&c, res[i]);
+	}
+
+	c.PrintTree(&c);
+	c.min(&c);
+	c.max(&c);
+
+
 	system("pause");
 	return 0;
 }
